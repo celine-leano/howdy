fba2ad421716adb4db907a9c3604808f05b74500
Celine Leano <cemleano@gmail.com>
2019-01-09 13:38:17 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+// turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// require autoload
+require_once('vendor/autoload.php');
+
+// create an instance of the Base class
+$f3 = Base::instance();
+
+// turn on Fat-Free error reporting
+$f3->set('DEBUG', 3);
+
+// define a default route
+$f3->route('GET /', function() {
+    echo '<h1>Howdy!</h1>';
+
+    $view = new View;
+});
+
+// run fat free
+$f3->run();
